3f4e8cd887ac3fa8e72456f73e42100938a67fd1
Michael van Niekerk <mvniekerk@chazaqdev.com>
2019-06-18 10:16:50 +0200

    0.9.2 beta (#17)
    * First 0.9.2-beta build
    
    * Fix copy of library because of wrong versions
    
    * CircleCI timeout to 20min
    
    * Remove tabs
    
    * Remove tab from command
    
    * Put command under run

--- container/linuxdeploy.sh
+++ container/linuxdeploy.sh
@@ -4,7 +4,7 @@
 case `uname -m` in
   "x86_64")
     ARCH="linux64"
-    FALLBACK_QT_ROOT=/opt/qt-5.9.6/5.9.6/gcc_64
+    FALLBACK_QT_ROOT=/opt/qt-5.12.3/5.12.3/gcc_64
     FALLBACK_QMAKE_ROOT="$FALLBACK_QT_ROOT/bin"
     ;;
 
@@ -26,20 +26,29 @@ QMAKE_CMD=qmake
 LOG="$PWD/linuxdeploy.log"
 QT_IFW_ROOT=/opt/qt-ifw-3.0.4
 
+# Detecting current pgModeler version
+DEPLOY_VER=`cat libutils/src/globalattributes.cpp | grep PgModelerVersion | sed 's/PgModelerVersion=QString("//g' | sed 's/"),//g' | sed 's/^ *//g' | cut -s -f2`
+
 STARTUP_SCRIPT="start-pgmodeler.sh"
 MIME_UPDATE_SCRIPT="dbm-mime-type.sh"
 ENV_VARS_SCRIPT="pgmodeler.vars"
 BUILD_DIR="$PWD/build"
 DIST_DIR="$PWD/dist"
 INSTALL_ROOT="/opt/pgmodeler"
-INSTALLER_CONF_DIR="$PWD/installer/linux/config"
-INSTALLER_PKG_DIR="$PWD/installer/linux/packages"
-INSTALLER_DATA_DIR="$INSTALLER_PKG_DIR/br.com.pgmodeler/data"
+FMT_PREFIX="\/opt\/pgmodeler"
+INSTALLER_APP_VER=`echo $DEPLOY_VER | cut -d '-' -f1`
+INSTALLER_CONF_DIR="$PWD/installer/template/config"
+INSTALLER_PKG_DIR="$PWD/installer/template/packages"
+INSTALLER_DATA_DIR="$INSTALLER_PKG_DIR/io.pgmodeler/data"
+INSTALLER_META_DIR="$INSTALLER_PKG_DIR/io.pgmodeler/meta"
+INSTALLER_TMPL_CONFIG="config.xml.tmpl"
+INSTALLER_CONFIG="config.xml"
+INSTALLER_TMPL_PKG_CONFIG="package.xml.tmpl"
+INSTALLER_PKG_CONFIG="package.xml"
 QT_CONF="$BUILD_DIR/$INSTALL_ROOT/qt.conf"
 DEP_PLUGINS_DIR="$BUILD_DIR/$INSTALL_ROOT/lib/qtplugins"
-
-# Detecting current pgModeler version
-DEPLOY_VER=`cat libutils/src/globalattributes.cpp | grep PGMODELER_VERSION | sed 's/PGMODELER_VERSION=QString("//g' | sed 's/"),//g' | sed 's/^ *//g' | cut -s -f2`
+BUILD_DATE=`date '+%Y-%m-%d'`
+  
 GEN_INSTALLER_OPT='-gen-installer'
 DEMO_VERSION_OPT='-demo-version'
 NO_QT_LIBS_OPT='-no-qt-libs'
@@ -78,7 +87,7 @@ QMAKE_ARGS="$QMAKE_ARGS \
             DOCDIR=$DOCDIR \
             SHAREDIR=$SHAREDIR \
             TEMPDIR=$TEMPDIR"
-
+            
 for param in $@; do
  if [[ "$param" == "$BUILD_ALL_OPT" ]]; then
   BUILD_ALL=1
@@ -91,13 +100,13 @@ for param in $@; do
  if [[ "$param" == "$COMPRESS_INSTALLER_OPT" ]]; then
    COMP_INST_PKG=1
  fi
-
+ 
  if [[ "$param" == "$DEMO_VERSION_OPT" ]]; then
    DEMO_VERSION=1
    GEN_INST_PKG=1
    QMAKE_ARGS="$QMAKE_ARGS DEMO_VERSION+=true"
  fi
-
+ 
  if [[ "$param" == "$NO_QT_LIBS_OPT" ]]; then
   BUNDLE_QT_LIBS=0
  fi
@@ -119,7 +128,7 @@ PKGFILE=$PKGNAME.tgz
 
 if [ $BUNDLE_QT_LIBS = 0 ]; then
   PKGFILE=$PKGNAME.tgz
-else
+else 
   #Dependency qt plugins copied to build dir
   DEP_PLUGINS="imageformats/libqgif.so \
                imageformats/libqico.so \
@@ -146,21 +155,21 @@ else
            libicudata.so.6*"
 fi
 
-clear
-echo
+clear 
+echo 
 echo "pgModeler Linux deployment script"
 echo "PostgreSQL Database Modeler Project - pgmodeler.io"
-echo "Copyright 2006-2018 Raphael A. Silva <raphael@pgmodeler.io>"
+echo "Copyright 2006-2019 Raphael A. Silva <raphael@pgmodeler.io>"
 
 # Identifying System Qt version
 if [ -e "$QMAKE_ROOT/$QMAKE_CMD" ]; then
-  QT_VER_1=`$QMAKE_ROOT/$QMAKE_CMD --version | grep --color=never -m 1 -o '[0-9].[0-9].[0-9]'`
+  QT_VER_1=`$QMAKE_ROOT/$QMAKE_CMD --version | grep --color=never -m 1 -o -E '[0-9].[0-9]+\.[0-9]+'`
   QT_VER_1=${QT_VER_1:0:5}
 fi
 
 # Identifying Fallback Qt version
 if [ -e "$FALLBACK_QMAKE_ROOT/$QMAKE_CMD" ]; then
-  QT_VER_2=`$FALLBACK_QMAKE_ROOT/$QMAKE_CMD --version | grep --color=never -m 1 -o '[0-9].[0-9].[0-9]'`
+  QT_VER_2=`$FALLBACK_QMAKE_ROOT/$QMAKE_CMD --version | grep --color=never -m 1 -o -E '[0-9].[0-9]+\.[0-9]+'`
   QT_VER_2=${QT_VER_2:0:5}
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$FALLBACK_QT_ROOT/lib"
 fi
@@ -175,13 +184,13 @@ else
 
   # Checking if identified versions are valid (>= 5.0.0)
   if [[ "$QT_VER_1" < "5.0.0" ]]; then
-
+  
      if [[ "$QT_VER_2" < "5.0.0" ]]; then
-
-	if [ -z $QT_VER_2 ]; then
-	  QT_VER_2="not found"
+	
+	if [ -z $QT_VER_2 ]; then 
+	  QT_VER_2="not found" 
 	fi
-
+	
 	echo
 	echo "** Qt framework found but in no suitable version (>= 5.0.0)!"
 	echo "** System Qt version: $QT_VER_1"
@@ -204,7 +213,7 @@ fi
 
 if [ $GEN_INST_PKG = 1 ]; then
   echo "The installer will be generated. (Found $GEN_INSTALLER_OPT)"
-
+  
   if [ $COMP_INST_PKG = 1 ]; then
     echo "The installer will be compressed (Found $COMPRESS_INSTALLER_OPT)"
   fi
@@ -237,7 +246,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Compiling code..."
-make -j8  
+make -j8  >> $LOG 2>&1
 
 if [ $? -ne 0 ]; then
   echo
@@ -247,7 +256,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Installing dependencies..."
-make install INSTALL_ROOT=$BUILD_DIR 
+make install INSTALL_ROOT=$BUILD_DIR  >> $LOG 2>&1
 
 if [ $? -ne 0 ]; then
   echo
@@ -258,9 +267,9 @@ fi
 
 if [ $BUNDLE_QT_LIBS = 1 ]; then
  echo "Copying Qt core libraries..."
-
+ 
  QT_ROOT=`$QMAKE_ROOT/qtpaths --install-prefix`  >> $LOG 2>&1
-
+ 
  LIB_DIR=
  MULTIARCH=`gcc -print-multiarch` >> $LOG 2>&1
  if [ -n "$MULTIARCH" ]; then
@@ -270,16 +279,16 @@ if [ $BUNDLE_QT_LIBS = 1 ]; then
  for lib in $QT_LIBS; do
   cp -v $QT_ROOT/lib/$LIB_DIR$lib $BUILD_DIR/$INSTALL_ROOT/lib >> $LOG 2>&1
  done
-
+ 
  if [ $? -ne 0 ]; then
   echo
   echo "** Library copy failed!"
   echo
   exit 1
  fi
-
+ 
  echo "Copying Qt plugins..."
-
+ 
  #Creates the file build/qt.conf to bind qt plugins
  mkdir $DEP_PLUGINS_DIR
  echo "[Paths]" > $QT_CONF
@@ -291,7 +300,7 @@ if [ $BUNDLE_QT_LIBS = 1 ]; then
  QT_PLUGIN_ROOT=`$QMAKE_ROOT/qtpaths --plugin-dir` >> $LOG 2>&1
  for plug in $DEP_PLUGINS; do
    pdir=`dirname $plug`
-
+   
    if [ -e $QT_PLUGIN_ROOT/$plug ]; then
         mkdir -p $DEP_PLUGINS_DIR/$pdir >> $LOG 2>&1
         cp -v $QT_PLUGIN_ROOT/$plug $DEP_PLUGINS_DIR/$pdir >> $LOG 2>&1
@@ -302,7 +311,7 @@ if [ $BUNDLE_QT_LIBS = 1 ]; then
             echo
             exit 1
         fi
-   fi
+   fi     
  done
 
 fi
@@ -334,7 +343,7 @@ if [ $DEMO_VERSION = 0 ]; then
   rm -r $PKGNAME  >> $LOG 2>&1
   mkdir $BUILD_DIR/$PKGNAME  >> $LOG 2>&1
   cp -r $BUILD_DIR/$INSTALL_ROOT/* $BUILD_DIR/$PKGNAME  >> $LOG 2>&1
-
+  
   cd $BUILD_DIR >> $LOG 2>&1
   tar -zcvf $PKGFILE $PKGNAME  >> $LOG 2>&1
   rm -r $PKGNAME  >> $LOG 2>&1
@@ -348,31 +357,42 @@ if [ $DEMO_VERSION = 0 ]; then
   fi
 
   mv $BUILD_DIR/$PKGFILE $DIST_DIR  >> $LOG 2>&1
-
+  
   if [ $? -ne 0 ]; then
     echo
     echo "** Failed to move $PKGFILE to $DIST_DIR"
     echo
     exit 1
-  fi
-
+  fi   
+  
   echo "File created: dist/$PKGFILE"
 fi
 
 
 if [ $GEN_INST_PKG = 1 ]; then
   echo "Generating installer..."
-
+ 
   rm $INSTALLER_DATA_DIR >> $LOG 2>&1
   ln -sf "$BUILD_DIR/$INSTALL_ROOT" $INSTALLER_DATA_DIR >> $LOG 2>&1
-
+   
   if [ $? -ne 0 ]; then
     echo
     echo "** Failed to configure installer data dir!"
     echo
     exit 1
-  fi
-
+  fi   
+ 
+  # Configuing installer scripts before packaging
+  cat $INSTALLER_CONF_DIR/$INSTALLER_TMPL_CONFIG | sed -e "s/{version}/$INSTALLER_APP_VER/g" | sed -e "s/{prefix}/$FMT_PREFIX/g" > $INSTALLER_CONF_DIR/$INSTALLER_CONFIG
+  
+  if [ $? -ne 0 ]; then
+    echo
+    echo "** Failed to create the installer config file!"
+    echo
+    exit 1
+  fi 
+   
+  # Packaging installation
   $QT_IFW_ROOT/bin/binarycreator -v -c $INSTALLER_CONF_DIR/config.xml -p $INSTALLER_PKG_DIR "$DIST_DIR/$PKGNAME.run" >> $LOG 2>&1
 
  if [ $? -ne 0 ]; then
@@ -381,13 +401,13 @@ if [ $GEN_INST_PKG = 1 ]; then
    echo
    exit 1
  fi
-
+ 
  if [ $COMP_INST_PKG = 1 ]; then
    _PWD=`pwd`
    cd $DIST_DIR  >> $LOG 2>&1
    tar -zcvf $PKGNAME.run.tgz $PKGNAME.run >> $LOG 2>&1
    rm $PKGNAME.run >> $LOG 2>&1
-
+   
     if [ $? -ne 0 ]; then
        echo
        echo "** Failed to create compressed installer!"
@@ -396,10 +416,10 @@ if [ $GEN_INST_PKG = 1 ]; then
     fi
     echo "File created: dist/$PKGNAME.run.tgz"
     cd $_PWD >> $LOG 2>&1
- else
+ else 
     echo "File created: dist/$PKGNAME.run"
  fi
-
+ 
 
 fi
 
@@ -408,8 +428,8 @@ echo
 
 if [ $BUILD_ALL = 1 ]; then
     if [ $COMP_INST_PKG = 1 ]; then
-        linuxdeploy.sh $DEMO_VERSION_OPT $COMPRESS_INSTALLER_OPT
+        ./linuxdeploy.sh $DEMO_VERSION_OPT $COMPRESS_INSTALLER_OPT
     else
-        linuxdeploy.sh $DEMO_VERSION_OPT
-    fi
+        ./linuxdeploy.sh $DEMO_VERSION_OPT 
+    fi    
 fi
